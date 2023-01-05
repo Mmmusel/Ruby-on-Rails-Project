9bed5bb11a86fac048ca04c31bb76887375f421f
musel <mmmusel@163.com>
2023-01-05 10:42:20 +0800

    1

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -23,9 +23,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if !User.select('*').where({'users.username' => @user.username}).empty?
-    
+    respond_to do |format|
     format.html { redirect_to new_user_url, notice: "用户名重复" }
        format.json { head :no_content }
+       end
         return
         end
 
